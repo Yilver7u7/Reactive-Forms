7acecdb1bb5b0c840d2a78cc722031aebab0b674
Yilver7u7 <yilversgg@gmail.com>
2024-10-14 15:06:56 -0500

    Add the sideBar linked the route with the path correctly

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+interface MenuItem{
+  tittle: string;
+  route: string;
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: ``
+})
+
+
+
+export class SideMenuComponent {
+
+
+  public reactiveMenu: MenuItem[] = [
+    { tittle: 'Básicos', route: './reactive/basic'},
+    { tittle: 'Dinámicos', route: './reactive/dynamic'},
+    { tittle: 'Switches', route: './reactive/switches'},
+  ]
+
+  public authMenu: MenuItem[] = [
+    { tittle: 'Register', route: './auth/sign-up'},
+  ]
+
+}
