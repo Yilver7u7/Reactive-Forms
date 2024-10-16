0dcd9978531f26c86e18e25bb40866ad40490a07
Yilver7u7 <yilversgg@gmail.com>
2024-10-16 18:55:46 -0500

    Add the validator on whole input

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -19,7 +19,13 @@ export class BasicPageComponent implements OnInit {
   //En el ciclo de vida de los componentes es preferible mediante el metodo reset
   //Reset the form
   ngOnInit(): void {
+    //Este metodo funciona siempre y cuando los atributos
+    //del objeto se encuentren dentro del form control
     this.myForm.reset(rtx5090);
+
+    //Esta funcion la puedes usar para comprobar que todas
+    //las validaciones estan funcionando
+    // this.myForm.markAllAsTouched()
   }
 
   public myForm: FormGroup = this.fb.group({
@@ -33,8 +39,42 @@ export class BasicPageComponent implements OnInit {
   onSave = () => {
     //Hacemos una pequeña verificacion de que este funcionando nuestro validador
     if( this.myForm.invalid ) return
+    this.myForm.markAllAsTouched();
     console.log( this.myForm.value );
   }
+
+  isValidField( field: string ): boolean | null{
+    //Mediante este metodo revisamos si los campos cuenta con algun error
+    //Es decir que no estan cumpliendo con las validaciones
+    return this.myForm.controls[ field ].errors
+    && this.myForm.controls[ field ].touched
+  }
+
+  getFieldError( field: string ): string | null{
+    //Hacemos una vaalidacion para saber si recibimos o no correctamen
+    //El field correspondiente entre los controls
+    if( !this.myForm.controls[field] ) return null;
+
+    //Asignamos el valor que nos envian en una variable
+    const errors = this.myForm.controls[field].errors ||{};
+
+    //Mediante la iteracion de los errores
+    //Podemos enviar un mensaje especifico
+    //Con cada uno de los errores de validacion que se presenten
+    for( const key of Object.keys(errors) ){
+      switch( key){
+        case 'required':
+           return 'Este campo es requerido';
+
+        case 'minlength':
+           return `Minimo ${ errors['minlength'].requiredLength} caracteres. `
+      }
+      console.log(key)
+    }
+    return null
+  }
+
+
 
   // Forma comun de hacer formularios reactivos
   // public myForm: FormGroup = new FormGroup({
