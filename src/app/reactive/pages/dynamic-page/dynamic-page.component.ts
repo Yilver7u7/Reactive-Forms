23dff2f47b9ded58d0bcef1252e490b5616d369b
Yilver7u7 <yilversgg@gmail.com>
2024-10-18 21:23:18 -0500

    TODO  que el placeholder o el valor del input muestre correctmente cada uno de los nombre de los items dentro del arreglo

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
-export class DynamicPageComponent {
+export class DynamicPageComponent implements OnInit {
+
+  constructor( private fb: FormBuilder ){}
+
+  ngOnInit(): void {
+    this.dynamicForm.reset();
+  }
+
+  public dynamicForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.maxLength(3) ], ],
+    //Trabajamos con un objeto para los que estaran listados en un campo
+
+    //Esto internamente crea un objeto que dentro de la propiedad controls
+    //Siendo estos llamados luego en el HTML median el metodo get
+    //Permite acceder al array y manipularlo
+    favoriteGames: this.fb.array([
+      [ 'Metal Gear', Validators.required ],
+      [ 'God of War', Validators.required ],
+      [ 'God of War', Validators.required ],
+    ])
+  })
+
+  get favoriteGames(){
+    //Esto es para que el sepa que se trata de un array y permita iterarlo
+    return this.dynamicForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit(): void{
+    if( this.dynamicForm.invalid ){
+      this.dynamicForm.markAllAsTouched();
+      return
+    }
+
+  }
+
 
 }
