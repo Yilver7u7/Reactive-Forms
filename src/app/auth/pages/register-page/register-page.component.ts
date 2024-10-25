120ad0b19b7e580184f2c9459b8f07062820d807
Yilver7u7 <yilversgg@gmail.com>
2024-10-25 11:05:03 -0500

    Started with the register

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name:['', [Validators.required, Validators.minLength(3)]],
+    email:['', [Validators.required, ]],
+    username:['', [Validators.required, ]],
+    password:['', [Validators.required, ]],
+    password2:['', [Validators.required, ]],
+  })
+
+  constructor( private fb: FormBuilder ){}
+
+  inValidField( field: string ){
+    //TODO: Obetener validacion desde un servicio
+  }
+
+  onSave(){
+    if( this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return
+    }
+  }
+
 
 }
