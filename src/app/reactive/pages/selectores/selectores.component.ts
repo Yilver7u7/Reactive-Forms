1a8dc6248c4027f6dbdea090ddbe93d4c61739d3
Yilver7u7 <yilversgg@gmail.com>
2024-10-30 10:11:16 -0500

    Build the UI into selectores

--- src/app/reactive/pages/selectores/selectores.component.ts
+++ src/app/reactive/pages/selectores/selectores.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selectores',
   templateUrl: './selectores.component.html',
 })
 export class SelectoresPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  })
+
+
+  constructor(
+    private fb: FormBuilder,
+  ){}
+
+  onSave(): void {
+    
+  }
 
 }
